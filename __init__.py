f126fc7ba88d449308e83e7e2e55eed9838f105b
Chinmay <cdandekar@ucsb.edu>
2024-04-16 23:29:23 +0000

    Added some styling and some input acceptance

--- __init__.py
+++ __init__.py
@@ -1,13 +1,19 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
 
     @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    def index():
+        return render_template('index.html', title='InstructScore Visualizer', text='../taurus/')
+    
+    @app.route('/process', methods=['POST'])
+    def process_input():
+        user_input = request.form['user_input']
+        processed_input = user_input.upper()  # Example: Convert input to uppercase
+        return render_template('result.html', processed_input=processed_input)
     
     return app
