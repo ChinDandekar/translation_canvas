f0e451be09138e4f1f5280b0b6892836b6c1670d
Chinmay <cdandekar@ucsb.edu>
2024-07-20 20:46:34 +0000

    Changed the ui a little

--- file_extraction_scripts/extract_pairs_from_tsv.py
+++ file_extraction_scripts/extract_pairs_from_tsv.py
@@ -25,11 +25,11 @@ def extract_pairs_from_tsv(file):
     ref_set = set()
     for index, row in translations.iterrows():
         if row['globalSegId'] not in id_set and row['system'] == 'HW-TSC':
-            pairs.append({'prediction': row['target'].replace('<v>', '').replace('</v>', '')})
+            pairs.append({'prediction': row['target']})
             id_set.add(row['globalSegId'])
             # Add only prediction and reference data to the pairs list
         if row['globalSegId'] not in ref_set and row['system'] == 'refA':
-            pairs[-1]['reference'] = row['target'].replace('<v>', '').replace('</v>', '')
+            pairs[-1]['reference'] = row['target']
             ref_set.add(row['globalSegId'])
     # raise Error
     return pairs
