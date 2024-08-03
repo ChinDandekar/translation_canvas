4f49b09c040c9b1789916dc86b5a1ef4d210f753
Chinmay <cdandekar@ucsb.edu>
2024-08-03 23:59:10 +0000

    changed tsv extraction script

--- file_extraction_scripts/extract_pairs_from_tsv.py
+++ file_extraction_scripts/extract_pairs_from_tsv.py
@@ -27,11 +27,11 @@ def extract_pairs_from_tsv(files: list[str]) -> list:
     src_set = set()
     for index, row in translations.iterrows():
         if row['globalSegId'] not in id_set and row['system'] == 'GPT4-5shot':
-            pairs.append({'prediction': row['target'].replace("<v>", "").replace("</v>", ""), 'source': row['source']})
+            pairs.append({'prediction': row['target']})
             id_set.add(row['globalSegId'])
             # Add only prediction and reference data to the pairs list
         if row['globalSegId'] not in ref_set and row['system'] == 'refA':
-            pairs[-1]['reference'] = row['target'].replace("<v>", "").replace("</v>", "")
+            pairs[-1]['reference'] = row['target']
             ref_set.add(row['globalSegId'])
     # raise Error
     return pairs
